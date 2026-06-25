9c1f414d51d9ce12bf85a9a02ea617c2c601b501
MonicaNSTorres <monicanstorress@gmail.com>
2026-06-25 14:44:16 -0300

    corrigindo build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,20 @@
-import PrismaClient from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma?: InstanceType<typeof PrismaClient.PrismaClient>;
+    prisma?: any;
 };
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
+    connectionString: process.env.DATABASE_URL!,
 });
 
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient.PrismaClient({ adapter });
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        adapter,
+    });
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+    globalForPrisma.prisma = prisma;
 }
